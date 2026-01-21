64c0e025a1a2f2dd65f6541a04c64dffbe8fcd85
DAMMAK Ayoub <ayoub.dammak@sagemcom.com>
2026-01-21 14:35:24 +0100

    Add Authentification to Swagger UI

--- Server_Side/AuthService/AuthService.API/Program.cs
+++ Server_Side/AuthService/AuthService.API/Program.cs
@@ -1,14 +1,28 @@
 ﻿using AuthService.Infrastructure.Data;
 using AuthService.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddOpenApi(); 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", document)] = []
+    });
+});
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -48,8 +62,6 @@ if (app.Environment.IsDevelopment())
         }
         await next();
     });
-
-    app.MapOpenApi();
 }
 
 app.UseCors();
